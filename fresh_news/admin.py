d440fcf5ea7ca07c24435a608bf2f3bd6a7c8806
asdadaversa <peikqudakeus@gmail.com>
2024-05-31 18:03:38 +0300

    update models.py, run migrations

--- fresh_news/admin.py
+++ fresh_news/admin.py
@@ -6,7 +6,7 @@ from fresh_news.models import News
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ("title", "publish_date", )
-    fields = ("title", "text", "publish_date", )
+    fields = ("title", "text", )
     search_fields = ("title", )
     list_filter = ("publish_date", )
     save_on_top = True
--- fresh_news/models.py
+++ fresh_news/models.py
@@ -4,9 +4,12 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    publish_date = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    from_source = models.URLField(blank=True, null=True)
 
     class Meta:
+        verbose_name = "News"
+        verbose_name_plural = "News"
         ordering = ["publish_date"]
 
     def __str__(self) -> str:
