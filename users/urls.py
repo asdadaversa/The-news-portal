5703d2087095ddee98957d87e5edc27c309c2e69
asdadaversa <peikqudakeus@gmail.com>
2024-06-13 18:14:36 +0300

    Create user endpoints: create, manage, activation. Username, passwords set, reset, reset confirm

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from users.views import CreateUserView, ManageUserView
-
+from users.views import UserActivationView, UserMeView, CreateUserView, UserPasswordView, UserUsernameView
 
 urlpatterns = [
-    path("", CreateUserView.as_view(), name="register"),
-    path("me/", ManageUserView.as_view(), name="manage"),
+    path("", CreateUserView.as_view({"post": "perform_create"}), name="your_create_user"),
+    path("me/", UserMeView.as_view(), name="manage"),
+
+    path("activation/<uid>/<token>/", UserActivationView.as_view({"post": "activation"}), name="user-activation"),
+    path("resend_activation/", UserActivationView.as_view({"post": "resend_activation"}), name="user-resend-activation"),
+
+    path("set_password/", UserPasswordView.as_view({"post": "set_password"}), name="user-set-password"),
+    path("reset_password/", UserPasswordView.as_view({"post": "reset_password"}), name="user-reset-password"),
+    path("reset_password_confirm/<uid>/<token>/", UserPasswordView.as_view({"post": "reset_password_confirm"}), name="user-reset-password-confirm"),
+
+    path("set_username/", UserUsernameView.as_view({"post": "set_username"}), name="user-set-username"),
+    path("reset_username/", UserUsernameView.as_view({"post": "reset_username"}), name="user-reset-username"),
+    path("reset_username_confirm/<uid>/<token>/", UserUsernameView.as_view({"post": "reset_username_confirm"}), name="user-reset-username-confirm"),
 ]
 
 app_name = "user"
--- users/views.py
+++ users/views.py
@@ -1,18 +1,214 @@
-from rest_framework import generics
-from rest_framework.authentication import SessionAuthentication
+import datetime
 
+from rest_framework import status, generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from djoser.compat import get_user_email
+from django.contrib.auth import update_session_auth_hash
+from djoser import utils
+from djoser.conf import settings
+
+from djoser.views import UserViewSet
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UpdateUserSerializer
 
 
-class CreateUserView(generics.CreateAPIView):
+class CreateUserView(UserViewSet):
     serializer_class = UserSerializer
 
+    def perform_create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        user.is_active = False
+        user.save()
 
-class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+        context = {"user": user}
+        to = [get_user_email(user)]
+
+        if settings.SEND_ACTIVATION_EMAIL:
+            settings.EMAIL.activation(self.request, context).send(to)
+        elif settings.SEND_CONFIRMATION_EMAIL:
+            settings.EMAIL.confirmation(self.request, context).send(to)
+
+        return Response(
+            {"message": "Registration was successful. The activation link has been sent to your email."},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class UserMeView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-    authentication_classes = [SessionAuthentication]
+    serializer_class = UpdateUserSerializer
 
     def get_object(self):
         return self.request.user
+
+
+class UserActivationView(UserViewSet):
+    @action(["post"], detail=False)
+    def activation(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.user
+        user.is_active = True
+        user.save()
+
+        if settings.SEND_CONFIRMATION_EMAIL:
+            context = {"user": user}
+            to = [get_user_email(user)]
+            settings.EMAIL.confirmation(self.request, context).send(to)
+
+        return Response(
+            {"message": "Account has been activated"},
+            status=status.HTTP_204_NO_CONTENT)
+
+    @action(["post"], detail=False)
+    def resend_activation(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.get_user(is_active=False)
+
+        if not settings.SEND_ACTIVATION_EMAIL:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        if not user:
+            return Response(
+                {"message": "User with this email does not exist."},
+                status=status.HTTP_204_NO_CONTENT
+            )
+
+        if user:
+            context = {"user": user}
+            to = [get_user_email(user)]
+            settings.EMAIL.activation(self.request, context).send(to)
+
+        return Response(
+            {"message": "The activation link has been resent to your email."},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
+class UserPasswordView(UserViewSet):
+    @action(["post"], detail=False)
+    def set_password(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        self.request.user.set_password(serializer.data["new_password"])
+        self.request.user.save()
+
+        if settings.PASSWORD_CHANGED_EMAIL_CONFIRMATION:
+            context = {"user": self.request.user}
+            to = [get_user_email(self.request.user)]
+            settings.EMAIL.password_changed_confirmation(self.request, context).send(to)
+
+        if settings.LOGOUT_ON_PASSWORD_CHANGE:
+            utils.logout_user(self.request)
+        elif settings.CREATE_SESSION_ON_LOGIN:
+            update_session_auth_hash(self.request, self.request.user)
+        return Response(
+            {"message": "The password was changed."},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+    @action(["post"], detail=False)
+    def reset_password(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.get_user()
+
+        if not user:
+            return Response(
+                {"message": "User with this email does not exist."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        context = {"user": user}
+        to = [get_user_email(user)]
+        settings.EMAIL.password_reset(self.request, context).send(to)
+
+        return Response(
+            {"message": "The reset password link has been sent to your email."},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+    @action(["post"], detail=False)
+    def reset_password_confirm(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.user.set_password(serializer.data["new_password"])
+        if hasattr(serializer.user, "last_login"):
+            serializer.user.last_login = datetime.datetime.now()
+        serializer.user.save()
+
+        if settings.PASSWORD_CHANGED_EMAIL_CONFIRMATION:
+            context = {"user": serializer.user}
+            to = [get_user_email(serializer.user)]
+            settings.EMAIL.password_changed_confirmation(self.request, context).send(to)
+        return Response(
+            {"message": "Password was changed successfully."},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
+class UserUsernameView(UserViewSet):
+
+    @action(["post"], detail=False, url_path=f"set_{User.USERNAME_FIELD}")
+    def set_username(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.request.user
+        new_username = serializer.data["new_" + User.USERNAME_FIELD]
+
+        setattr(user, User.USERNAME_FIELD, new_username)
+        user.save()
+        if settings.USERNAME_CHANGED_EMAIL_CONFIRMATION:
+            context = {"user": user}
+            to = [get_user_email(user)]
+            settings.EMAIL.username_changed_confirmation(self.request, context).send(to)
+        return Response(
+            {"message": "The username was changed."},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+    @action(["post"], detail=False, url_path=f"reset_{User.USERNAME_FIELD}")
+    def reset_username(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.get_user()
+
+        if not user:
+            return Response(
+                {"message": "User with this email does not exist."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        context = {"user": user}
+        to = [get_user_email(user)]
+        settings.EMAIL.username_reset(self.request, context).send(to)
+
+        return Response(
+            {"message": "The reset email link has been sent to your email."},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+    @action(["post"], detail=False, url_path=f"reset_{User.USERNAME_FIELD}_confirm")
+    def reset_username_confirm(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        new_username = serializer.data["new_" + User.USERNAME_FIELD]
+
+        setattr(serializer.user, User.USERNAME_FIELD, new_username)
+        if hasattr(serializer.user, "last_login"):
+            serializer.user.last_login = datetime.datetime.now()
+        serializer.user.save()
+
+        if settings.USERNAME_CHANGED_EMAIL_CONFIRMATION:
+            context = {"user": serializer.user}
+            to = [get_user_email(serializer.user)]
+            settings.EMAIL.username_changed_confirmation(self.request, context).send(to)
+        return Response(
+            {"message": "Username was changed successfully."},
+            status=status.HTTP_204_NO_CONTENT
+        )
